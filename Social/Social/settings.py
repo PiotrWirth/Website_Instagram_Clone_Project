6ae5a5008edfe9a3694640127b9025519370d894
Piotr <piotr.wirth@onet.pl>
2023-01-29 16:18:31 +0100

    commit

--- Social/Social/settings.py
+++ Social/Social/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Social.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
--- Social/accounts/views.py
+++ Social/accounts/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from .forms import LoginForm
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def user_login(request):
-    form = LoginForm()
-    return render(request,'users/login.html',{'form':form})
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(request,username=data['username'],password=data['password'])
+            if user is not None:
+                login(request,user)
+                return HttpResponse('user authenticated and logged in')
+            else:
+                return HttpResponse('Invalid credentials')
+
+    else:
+        form = LoginForm()
+    return render(request,'login.html',{'form':form})
+    
+@login_required
+def index(request):
+    return render(request, 'index.html')
